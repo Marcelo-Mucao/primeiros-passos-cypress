6245931146dd844cd08d811e090d5f17734f0f80
Marcelo Mucao <mucaomarcelo@gmail.com>
2025-07-07 21:34:56 -0300

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -30,13 +30,15 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.firstNameField).clear().type("FirstNameTest")
     cy.get(selectorsList.lastNameField).clear().type("LastNameTest")
     cy.get(selectorsList.genericField).eq(3).clear().type("Employee")
-    cy.get(selectorsList.genericField).eq(4).clear().type("otherIDTeste")
-    cy.get(selectorsList.genericField).eq(5).clear().type("DriversNumberTeste")
+    cy.get(selectorsList.genericField).eq(4).clear().type("otherIdTeste")
+    cy.get(selectorsList.genericField).eq(5).clear().type("DriversLicenseTeste")
     cy.get(selectorsList.genericField).eq(6).clear().type("2025-03-10")
     cy.get(selectorsList.dateCloseButton).click()
     cy.get(selectorsList.submitButton).eq(0).click()
     cy.get("body").should("contain", "Successfully Updated")
-  })
+    cy.get('.oxd-toast-close')
+    
+   })
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
