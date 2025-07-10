a80bd26afa599e4fe3a29fb2890612093e96ad41
Marcelo Mucao <mucaomarcelo@gmail.com>
2025-07-10 11:19:03 -0300

    Add page objects part one

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,29 +1,30 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+
+const loginPage = new LoginPage()
 
 describe('Orange HRM Tests', () => {
   
-  const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
+  const selectorsList = {    
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
+    dashboardGrid: ".orangehrm-dashboard-grid",    
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
+    genericCombobox:".oxd-select-text--arrow",
+    secondItemCombobox:".oxd-select-dropdown > :nth-child(3)",
+    ThirdItemCombobox:".oxd-select-dropdown > :nth-child(2)",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
+
   }
     
   it.only('User Info Update - Success', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSucces.username,userData.userSucces.password)
     
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucces.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucces.password)
-    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click()
@@ -34,10 +35,14 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.genericField).eq(5).clear().type("DriversLicenseTeste")
     cy.get(selectorsList.genericField).eq(6).clear().type("2025-03-10")
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get(selectorsList.submitButton).eq(0).click({force:true})
     cy.get("body").should("contain", "Successfully Updated")
     cy.get('.oxd-toast-close')
-    
+    cy.get(selectorsList.genericCombobox).eq(0).click()
+    cy.get(selectorsList.secondItemCombobox).click()
+    cy.get(selectorsList.genericCombobox).eq(1).click()
+    cy.get(selectorsList.ThirdItemCombobox).click()
+
    })
   it('Login - Fail', () => {
     cy.visit('/auth/login')
